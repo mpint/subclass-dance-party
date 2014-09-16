14ad783fe4daef6954aed999a990c3273bbf14e1
mpint <mpinter09@gmail.com>
2014-09-15 19:04:46 -0700

    completed circledancer class (VERY NICE)

---
+++ src/circleDancer.js
@@ -0,0 +1,12 @@
+var CircleDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+};
+
+CircleDancer.prototype = Object.create(Dancer.prototype);
+CircleDancer.prototype.constructor = CircleDancer;
+
+CircleDancer.prototype.step = function() {
+
+  Dancer.prototype.step.call(this);
+  this.$node.animate({left: Math.random()*500, top: Math.random()*800});
+};
