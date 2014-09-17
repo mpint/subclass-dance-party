751b67630cb96b114a2cb355bc32c14320759bf9
mpint <mpinter09@gmail.com>
2014-09-16 19:17:04 -0700

    Cleaned up spinDancer object

--- src/spinDancer.js
+++ src/spinDancer.js
@@ -9,16 +9,6 @@ SpinDancer.prototype.step = function() {
   var colors = ['40px solid blue', '60px dotted green', '30px double purple'];
 
   Dancer.prototype.step.call(this);
-
-  this.setBorderColor(colors[(Math.floor(Math.random() * 3))]);
-};
-
-SpinDancer.prototype.setBorderColor = function(border) {
-
-  var styleSetting = {
-    border: border
-  };
-
-  this.$node.css(styleSetting);
-  this.$node.addClass('rotate');
+  this.$node.css({'border': colors[(Math.floor(Math.random() * 3))]})
+            .addClass('rotate');
 };
